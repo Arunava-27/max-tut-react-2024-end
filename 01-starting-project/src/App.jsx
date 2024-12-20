4afc9447940f53b2f4bef0d13ff2c75cb3b95037
Arunava-27062003 <arunava.kundu@iemlabs.com>
2024-12-20 14:34:48 +0530

    refactor: restructure App component and create Header and CoreConcepts components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,9 @@
-import reactImg from "./assets/react-core-concepts.png";
+
 import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
+
+import Header from "./components/Header";
+import CoreConcepts from "./components/CoreConcepts";
 
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 
@@ -8,42 +11,7 @@ function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function Header() {
-  const description =
-    reactDescription[genRandomInt(reactDescription.length - 1)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 
-function CoreConcepts(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
-
-// The above function can also be written as below using destructuring
-
-// function CoreConcepts({ image, title, description }) {
-//   return (
-//     <li>
-//       <img src={image} alt={title} />
-//       <h3>{title}</h3>
-//       <p>{description}</p>
-//     </li>
-//   );
-// }
 
 function App() {
   return (
@@ -64,7 +32,9 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+
+        </section>
       </main>
     </div>
   );
